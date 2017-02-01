73d4456ac48405ea7071e92054469518f0abf2da
edcepp <epp@zdome.net>
2017-02-01 12:15:55 -0800

    implement display the volume

--- app/src/main/java/net/zdome/cs371/epp/seekbardemo/MainActivity.java
+++ app/src/main/java/net/zdome/cs371/epp/seekbardemo/MainActivity.java
@@ -2,12 +2,34 @@ package net.zdome.cs371.epp.seekbardemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView volume_display;
+    private SeekBar  volume_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        volume_bar = (SeekBar)findViewById(R.id.volume_bar);
+        volume_bar.setOnSeekBarChangeListener (new VolumeListener());
+        volume_display = (TextView)findViewById(R.id.volume_display);
+    }
+
+    private class VolumeListener implements SeekBar.OnSeekBarChangeListener {
+        public void onProgressChanged (SeekBar s, int volume, boolean b){
+            int volumn = volume_bar.getProgress();
+            String volume_string = Integer.toString(volume);
+            volume_display.setText(volume_string);
+        }
+
+        public void onStartTrackingTouch (SeekBar s){}
+        public void onStopTrackingTouch (SeekBar s){}
+
     }
 }
